15e728d37ab0d3340d3ce72b2ca43fc7c85428de
Umair Hanif <umair.hanif@ropstam.com>
2022-08-22 12:03:19 +0500

    Initial Commit

---
+++ Billionaires/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Billionaires
+//
+//  Created by Umair Hanif on 22/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
